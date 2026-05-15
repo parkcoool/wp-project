c8733dfb1b5b813afbdb8a5771103b8bd18e4d07
lch2023 <lch2023@naver.com>
2026-05-15 15:22:29 +0900

    Design: 스테이지 선택 모달창 UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  showScreen('menu-screen');
+
+  const modalOpenBtn = document.querySelectorAll('.main-menu-btn');
+  const modalBoxes = document.querySelectorAll('.main-menu-modal');
+  modalOpenBtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      modalBoxes[index - 1]?.classList.add('active');
+    });
+  });
+
+  const modalCloseBtn = document.querySelectorAll('.main-menu-modal-close-btn');
+  modalCloseBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      btn.parentElement.classList.remove('active');
+    });
+  });
+});
+
 const showScreen = (screenId) => {
   const screen = document.querySelector(`.screen#${screenId}`);
   if (screen) {
@@ -7,5 +26,3 @@ const showScreen = (screenId) => {
     screen.classList.add('active');
   }
 };
-
-showScreen('menu-screen');
