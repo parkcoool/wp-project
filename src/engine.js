f2ac7c067ef066d312ddc6b595614c3a449f2287
김동규 <dong04kd@gmail.com>
2026-05-24 23:12:12 +0900

    engine.js 수정

--- src/engine.js
+++ src/engine.js
@@ -2,6 +2,7 @@ import { APPEARANCE_PRESETS, GameState, CANVAS_LAYOUT } from "./state.js";
 import { onBrickHit } from "./stageManager.js";
 import { onBallLaunch, onBallMiss } from "./fuelSystem.js";
 import { playSoundEffect } from "./audio.js";
+import { updateItems } from "./itemSkill.js";
 
 let canvas;
 let ctx;
